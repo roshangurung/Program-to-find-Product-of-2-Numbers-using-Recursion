0973092ac61ee84f847606dac892c52a9de9e0b9
roshangurung <r.gurung1992@gmail.com>
2019-12-10 21:23:47 -0800

    Initial commit

---
+++ Program to find Product of 2 Numbers using Recursion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Program_to_find_Product_of_2_Numbers_using_Recursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
